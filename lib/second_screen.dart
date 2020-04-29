b384d3269222598bb167e9ebd05cebada87c928b
Skalozub <norton47@yandex.by>
2020-04-29 10:49:01 +0300

    fix with material app and navigation

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,26 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:async';
+import 'dart:convert';
+
+class HttpService {
+  final String postsURL = "https://jsonplaceholder.typicode.com/posts";
+
+  Future<List<Post>> getPosts() async {
+    http.Response res = await http.get(postsURL);
+
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+
+      List<Post> posts = body
+          .map(
+            (dynamic item) => Post.fromJson(item),
+          )
+          .toList();
+
+      return posts;
+    } else {
+      throw "Can't get posts.";
+    }
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({
+    @required this.userId,
+    @required this.id,
+    @required this.title,
+    @required this.body,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+      body: json['body'] as String,
+    );
+  }
+}
 
 class SecondScreen extends StatelessWidget {
+  final HttpService httpService = HttpService();
+
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: Text("Second screen")),
-      body: Center(
-          child: Column(
-        children: <Widget>[
-          RaisedButton(
-            onPressed: () {
-              Navigator.of(context).pop("42");
-            },
-            child: const Text('Return 42'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              Navigator.of(context).pop("AbErVaLlG");
-            },
-            child: const Text('Return AbErVaLlG'),
-          )
-        ],
-      )),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Posts"),
+      ),
+      body: FutureBuilder(
+        future: httpService.getPosts(),
+        builder: (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
+          if (snapshot.hasData) {
+            List<Post> posts = snapshot.data;
+            return ListView(
+              children: posts
+                  .map(
+                    (Post post) => Card(
+                      child: ListTile(
+                        leading: FlutterLogo(size: 72.0),
+                        title: Text('${post.title}'),
+                        subtitle: Text('${post.body}'),
+                        trailing: RaisedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop("${post.id}");
+                          },
+                          child: Icon(Icons.more_vert),
+                        ),
+                        isThreeLine: true,
+                      ),
+                    ),
+                  )
+                  .toList(),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
